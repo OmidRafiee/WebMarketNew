4f37596767fdd6348f133948e1c71f14dea2d69a
OmidRafiee <omid.green.b13@gmail.com>
2017-06-14 21:20:25 +0430

    Add files via upload

---
+++ WebMarket.ViewModel/User/Product/ProductPriceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebMarket.DomainClasses.Entity;
+
+namespace WebMarket.ViewModel.User.Product
+{
+    public class ProductPriceViewModel
+    {
+        public int MaxPrice { get; set; }
+        public int MinPrice { get; set; }
+    }
+}
