a056403c489061131257c69397ad000f06f1bea0
OmidRafiee <omid.green.b13@gmail.com>
2017-06-14 21:17:07 +0430

    Add files via upload

---
+++ WebMarket.ServiceLayer/Interfaces/IEmailService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebMarket.DomainClasses.Entity;
+using WebMarket.ViewModel.Admin.Setting;
+
+namespace WebMarket.ServiceLayer.Interfaces
+{
+    public interface IEmailService
+    {
+        Task<bool> SendMailByAttach(string subject, string body, string attachment, params string[] toMails);
+        Task<bool> SendMailByAttach(string subject, string body, System.Net.Mail.Attachment attachment,
+                                         params string[] toMails);
+
+        Task<bool> SendMailByAttachList(string subject, string body, string toMail, params string[] attachments);
+
+        Task<bool> SendMailByAttachList2(string subject, string body, string toMail,
+            params System.Net.Mail.Attachment[] attachments);
+
+        Task<bool> SendMail(string smtp, string fromMail, string password, string fromName, string subject,
+                                                string body,
+                                                params string[] toMails);
+
+        Task < bool > SendMail ( string subject ,string body ,params string [ ] toMails );
+
+
+
+    }
+}
---
+++ WebMarket.ServiceLayer/Interfaces/IFactorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebMarket.DataLayer.Context;
+using WebMarket.DomainClasses.Entity;
+
+namespace WebMarket.ServiceLayer.Interfaces
+{
+    public interface IFactorService
+    {
+         IEnumerable <ViewModel.User.Factor.FactorViewModel> ListFactor();
+
+        IList < ViewModel.User.Factor.FactorViewModel > GetFactorData ( out int total ,
+                                                 int page ,
+                                                 int count ,
+                                                 DomainClasses.Enums.Payments payment );
+    }
+}
